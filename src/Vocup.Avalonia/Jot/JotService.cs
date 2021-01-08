f62a248f6f6ccf6b718dfa96f81781a1624fb7bf
Schafwolle <8407328+Schafwolle@users.noreply.github.com>
2021-01-08 22:47:50 +0100

    Added JotService as Jot Bootstrap

---
+++ src/Vocup.Avalonia/Jot/JotService.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using Jot;
+
+namespace Vocup.Avalonia.Jot
+{
+    /// <summary>
+    /// Static bootstrap for the Jot objects
+    /// </summary>
+    static class JotService
+    {
+
+        public static Tracker Tracker = new Tracker();
+
+        static JotService()
+        {
+            // configure tracking for all Form objects
+
+            Tracker
+                .Configure<Window>()
+                .Id(f => f.Name)
+                .Properties(f => new { f.Width, f.Height, f.Position, f.WindowState })
+                .PersistOn(nameof(Window.Closing))
+                //.WhenPersistingProperty((f, p) => p.Cancel = (f.WindowState != WindowState.Normal && (p.Property == nameof(Form.Height) || p.Property == nameof(Form.Width) || p.Property == nameof(Form.Top) || p.Property == nameof(Form.Left))))
+                .StopTrackingOn(nameof(Window.Closed));
+        }
+    }
+}
